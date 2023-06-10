5a22b38776fc912f2f16a5b822cf51f9ad4de035
Dawid Durołek <dawid.durolek@onet.pl>
2023-06-10 21:04:17 +0200

    seting_up_integration_test

---
+++ src/integration/java/com/junioroffers/future/JobOfferIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.junioroffers.future;
+
+import com.junioroffers.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class JobOfferIntegrationTest extends BaseIntegrationTest {
+    @Test
+    public void first_integration_test() {
+    }
+
+}
--- src/main/java/com/junioroffers/JobOffersApplication.java
+++ src/main/java/com/junioroffers/JobOffersApplication.java
@@ -1,13 +1,13 @@
-package com.junioroffers.domain;
+package com.junioroffers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JobOffersApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(JobOffersApplication.class, args);
   }
 
 }
